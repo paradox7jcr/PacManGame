22915b2d2a0937954a70fe53a8cc72f6cfbc49fd
Qinqi "Bill" Wang <qinqi402@gmail.com>
2017-09-05 16:57:31 -0500

    Added controller movement, smoothed out environment

--- Assets/Player_Move_Prot.cs
+++ Assets/Player_Move_Prot.cs
@@ -15,6 +15,11 @@ public class Player_Move_Prot : MonoBehaviour
 
     public float moveY;
 
+    public float moveXPad;
+    public float moveYPad;
+    public float moveXStick;
+    public float moveYStick;
+	
     public Sprite spriteRight;
     public Sprite spriteLeft;
     public Sprite spriteUp;
@@ -39,23 +44,31 @@ public class Player_Move_Prot : MonoBehaviour
     {
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
+		
+		//Used for PS4 d-pad and leftStick controls
+        //TODO: Figure out how to deal with conflicting controls in
+        //a less clunky way than adding extra variables
+		moveXPad = Input.GetAxis("DPAD_X");
+        moveYPad = Input.GetAxis("DPAD_Y");
+        moveXStick = Input.GetAxis("Horizontal_Stick");
+        moveYStick = Input.GetAxis("Vertical_Stick");
 
-        if (moveX < 0.0f)
+        if (moveX < 0.0f || moveXPad < 0.0f || moveXStick < 0.0f)
         {
             TurnPlayer("left");
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-1*playerSpeed, 0.0f);
         }
-        else if (moveX > 0.0f)
+        else if (moveX > 0.0f || moveXPad > 0.0f || moveXStick > 0.0f)
         {
             TurnPlayer("right");
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed, 0.0f);
         }
-        if (moveY < 0.0f)
+        if (moveY < 0.0f || moveYPad < 0.0f || moveYStick < 0.0f)
         {
             TurnPlayer("down");
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, -1*playerSpeed);
         }
-        else if (moveY > 0.0f)
+        else if (moveY > 0.0f || moveYPad > 0.0f || moveYStick > 0.0f)
         {
             TurnPlayer("up");
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, playerSpeed);
