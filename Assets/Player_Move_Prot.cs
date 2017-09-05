740b8ac467de3e89e2cb3caba980dc38976fa685
Curtis Green <greencur000@email.tamu.edu>
2017-09-04 21:20:20 -0500

    Sprite changes on movement

--- Assets/Player_Move_Prot.cs
+++ Assets/Player_Move_Prot.cs
@@ -7,18 +7,34 @@ public class Player_Move_Prot : MonoBehaviour
     public int playerSpeed = 5;
 
     public bool facingRight = false;
-
     public bool facingUp = false;
+    public bool facingDown = false;
+    public bool facingLeft = false;
 
     public float moveX;
 
     public float moveY;
-	
-	// Update is called once per frame
-	void Update ()
+
+    public Sprite spriteRight;
+    public Sprite spriteLeft;
+    public Sprite spriteUp;
+    public Sprite spriteDown;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Update is called once per frame
+    void Update ()
 	{
-	    PlayerMove();
-	}
+        PlayerMove();
+        ChangePlayerSprite();
+    }
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+        if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+            spriteRenderer.sprite = spriteRight; // set the sprite to sprite1
+    }
 
     void PlayerMove()
     {
@@ -27,35 +43,69 @@ public class Player_Move_Prot : MonoBehaviour
 
         if (moveX < 0.0f)
         {
-            //FlipPlayerX();
+            TurnPlayer(1);
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-1*playerSpeed, 0.0f);
         }
         else if (moveX > 0.0f)
         {
-            //FlipPlayerX();
+            TurnPlayer(0);
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed, 0.0f);
         }
         if (moveY < 0.0f)
         {
-            //FlipPlayerY();
+            TurnPlayer(2);
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, -1*playerSpeed);
         }
         else if (moveY > 0.0f)
         {
-            //FlipPlayerY();
+            TurnPlayer(3);
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, playerSpeed);
         }
 
         
     }
 
-    void FlipPlayerX()
+void ChangePlayerSprite()
     {
-        facingRight = !facingRight;
+        if (facingRight)
+           spriteRenderer.sprite = spriteRight;
+        else if(facingLeft)
+            spriteRenderer.sprite = spriteLeft;
+        else if (facingUp)
+            spriteRenderer.sprite = spriteUp;
+        else
+            spriteRenderer.sprite = spriteDown;
     }
 
-    void FlipPlayerY()
+    void TurnPlayer(int x)
     {
-        facingUp = !facingUp;
+        if (x == 0)
+        {
+            facingRight = true;
+            facingLeft = false;
+            facingDown = false;
+            facingUp = false;
+        }
+        else if (x == 1)
+        {
+            facingRight = false;
+            facingLeft = true;
+            facingDown = false;
+            facingUp = false;
+        }
+        else if (x == 2)
+        {
+            facingRight = false;
+            facingLeft = false;
+            facingDown = true;
+            facingUp = false;
+        }
+        else
+        {
+            facingRight = false;
+            facingLeft = false;
+            facingDown = false;
+            facingUp = true;
+        }
     }
 }
